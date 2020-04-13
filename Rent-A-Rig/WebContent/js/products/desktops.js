36326566a5b98303da59bec1936fc59e8cbcd8f6
Justin <justin.thiele@hotmail.com>
2020-04-13 12:33:14 -0700

    saving to work on other branch

--- Rent-A-Rig/WebContent/js/products/desktops.js
+++ Rent-A-Rig/WebContent/js/products/desktops.js
@@ -7,4 +7,6 @@ $(document).ready(function(){
 	$('#ABS_Mage_M').load("../../html/Products/ABS_Mage_M.html");
 	$('#MSI_Trident_XPlus').load("../../html/Products/MSI_Trident_XPlus.html");
 	$('#ASUS_VG245H').load("../../html/Products/ASUS_VG245H.html");
+	$('#ACER_Z1_Z321QU').load("../../html/Products/ACER_Z1_Z321QU.html");
+	$('#ASUS_XG32VQ').load("../../html/Products/ASUS_XG32VQ.html");
 });
