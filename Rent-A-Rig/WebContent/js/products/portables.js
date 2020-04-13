f0b3c1d0ac556f605dbb75b9fbbd58c4c345c764
romyk <romyk@DESKTOP-98HI4FO>
2020-04-13 14:01:40 -0700

    commit- added portable gaming laptops and thin and light

---
+++ Rent-A-Rig/WebContent/js/products/portables.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('#MSI_9SF-240').load("../../html/Products/MSI_9SF-240.html");
+	$('#ASUS_ROG_Zephyrus_S').load("../../html/Products/ASUS_ROG_Zephyrus_S.html");
+	$('#Acer_PH315-52-71RT').load("../../html/Products/Acer_PH315-52-71RT.html");
+	$('#Microsoft_Surface_JKX-00001').load("../../html/Products/Microsoft_Surface_JKX-00001.html");
+	$('#MacBook_Air_MJVE2LLA').load("../../html/Products/MacBook_Air_MJVE2LLA.html");
+	$('#Lenovo_ThinkPad_20L7002HUS').load("../../html/Products/Lenovo_ThinkPad_20L7002HUS.html");
+	$('#Microsoft_Surface_JKX-00001').load("../../html/Products/Microsoft_Surface_JKX-00001.html");
+	$('#Microsoft_Surface_JKX-00001').load("../../html/Products/Microsoft_Surface_JKX-00001.html");
+	$('#Microsoft_Surface_JKX-00001').load("../../html/Products/Microsoft_Surface_JKX-00001.html");
+});
+
+
+
