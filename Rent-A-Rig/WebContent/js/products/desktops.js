dfa350856a57a9505c3dda1f595c65a88eb45ec0
Justin <justin.thiele@hotmail.com>
2020-04-13 13:16:23 -0700

    fixed issues where pages weren't loading

--- Rent-A-Rig/WebContent/js/products/desktops.js
+++ Rent-A-Rig/WebContent/js/products/desktops.js
@@ -1,12 +1,8 @@
-/**
- * 
- */
-
 $(document).ready(function(){
-	$('#RoG_Strix_GL12').load("../../html/Products/RoG_Strix_GL12.html");
-	$('#ABS_Mage_M').load("../../html/Products/ABS_Mage_M.html");
-	$('#MSI_Trident_XPlus').load("../../html/Products/MSI_Trident_XPlus.html");
-	$('#ASUS_VG245H').load("../../html/Products/ASUS_VG245H.html");
-	$('#ACER_Z1_Z321QU').load("../../html/Products/ACER_Z1_Z321QU.html");
-	$('#ASUS_XG32VQ').load("../../html/Products/ASUS_XG32VQ.html");
+	$('#RoG_Strix_GL12').load("Products/RoG_Strix_GL12.html");
+	$('#ABS_Mage_M').load("Products/ABS_Mage_M.html");
+	$('#MSI_Trident_XPlus').load("Products/MSI_Trident_XPlus.html");
+	$('#ASUS_VG245H').load("Products/ASUS_VG245H.html");
+	$('#ACER_Z1_Z321QU').load("Products/ACER_Z1_Z321QU.html");
+	$('#ASUS_XG32VQ').load("Products/ASUS_XG32VQ.html");
 });
